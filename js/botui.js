3f9857b3e9e77d3acd0dbcb7ceba0b0feb2a32b5
Dnaoatan1226 <819807093@qq.com>
2022-09-14 12:21:13 +0800

    Site updated: 2022-09-14 12:21:13

--- js/botui.js
+++ js/botui.js
@@ -1,29 +1,30 @@
-function bot_ui_ini() {
-	var botui = new BotUI("hello")
+<script src="https://cdn.bootcdn.net/ajax/libs/botui/0.3.9/botui.js"></script>
+function bot ui ini() {
+	var botui = new BotUI("hello-mashiro")
 	botui.message. add ({
 		delay: 800, 
 		content: "Hi, there!"
 	}).then(function () {
-	botui.message.add ({
+	  botui.message.add ({
 		delay: 1100,
 		content：“这里是 cungudafa”
-	}) . then(function () {
-		botui.message.add({
-			delay: 1100,
-			 content:"一个可爱的女孩子～”
+		}) . then(function () {
+		    botui.message.add({
+			delay: 1100, 
+   			content: "一个可爱的女孩子～”
 		}).then(function () {
 			botui.action.button({
-			delay: 1600, 
-			action: [{
+			delay: 1600,
+			 action: [{
 				text:"然后呢?"
 				value: "sure"
-				},{
-					text:"少废话"
-					value: "skip"
-				}]
-			}). then(function (a){
+			},{
+				text:"少度活！"
+				value: "skip"
+			}]
+		}). then(function (a){
 			"sure" == a.value && sure();
-			"skip" == a. value && end()
+			 "skip" == a. value && end()
 			})
 		})
 	})
